b22774ec4a4b9c8f1a3fdf0a201b37ee59cdb7a5
Kotov-Roman <poma_kotov@mail.ru>
2018-09-27 00:33:19 +0400

    Added package homework2

--- src/homework2/Task2.java
+++ src/homework2/Task2.java
@@ -1,6 +1,12 @@
 package homework2;
 
-public class Zadanie {
+public class Task2 {
+    /* 9.13. Given a word. Display third symbol.
+     * 9.14. Given a word. Display last symbol.
+     * 9.15. Given a word. Display symbol at number k.
+     * 9.64. Given a sentence. Determine how many identical symbols there are in it.
+     * 9.100. Given a word. Swap it's second and fifth parts.
+     */
 
     public static char getThirdChar(String string) {
         return string.charAt(2);
@@ -16,18 +22,19 @@ public class Zadanie {
 
     public static int getSummOfEqualsChars(String string) {
         int count = 0;
-        int summ = 0;
-        string.toCharArray();
+        int sum = 0;
         for (int i = 0; i < string.length() - 1; i++) {
             if (string.charAt(i) == string.charAt(i + 1)) {
                 count++;
             } else if (string.charAt(i) != string.charAt(i + 1) && count > 0) {
-                summ = summ + 1 + count;
-                System.out.println();
+                sum = sum + 1 + count;
                 count = 0;
             }
         }
-        return summ;
+        if (count != 0) {
+            sum = sum + 1 + count;
+        }
+        return sum;
     }
 
     public static String swapSecondAndFifth(String string) {
@@ -35,8 +42,19 @@ public class Zadanie {
         char temp = chars[1];
         chars[1] = chars[4];
         chars[4] = temp;
-        String result = new String(chars);
-        return result;
+        return String.copyValueOf(chars);
     }
+
+    public static String reverse(String string) {
+        char[] chars = string.toCharArray();
+        char[] result = new char[chars.length];
+        int resultIndex = 0;
+        for (int i = chars.length -1 ; i >= 0; i--) {
+            result[resultIndex] = chars[i];
+            resultIndex++;
+        }
+        return String.copyValueOf(result);
+    }
+
 
 }
