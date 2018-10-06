d7d58db03cdbde62609ba8fa39031181e26ee263
Kotov-Roman <19671970Hjvf>
2018-10-06 14:41:53 +0300

    corrcted Task 2 in package homework2

--- src/homework2/Task2.java
+++ src/homework2/Task2.java
@@ -9,52 +9,105 @@ public class Task2 {
      */
 
     public static char getThirdChar(String string) {
-        return string.charAt(2);
+        try {
+            if (string.length() >= 3) {
+                return string.charAt(2);
+            } else {
+                throw new IllegalArgumentException("Illegal argument");
+            }
+        } catch (NullPointerException e) {
+            System.err.println("String is null");
+        }
+        throw new IllegalArgumentException("Illegal argument");
     }
 
     public static char getLastChar(String string) {
-        return string.charAt(string.length() - 1);
+        try {
+            if (string.length() != 0) {
+                return string.charAt(string.length() - 1);
+            } else {
+                throw new IllegalArgumentException("Illegal argument");
+            }
+        } catch (NullPointerException e) {
+            System.err.println("String is null");
+        }
+        throw new IllegalArgumentException("Illegal argument");
     }
 
     public static char getTargetChar(String string, int target) {
-        return string.charAt(target - 1);
+        try {
+            if (string.length() >= target) {
+                return string.charAt(target - 1);
+            } else {
+                throw new IllegalArgumentException("Illegal argument");
+            }
+        } catch (NullPointerException e) {
+            System.err.println("String is null");
+        }
+        throw new IllegalArgumentException("Illegal argument");
     }
 
     public static int getSummOfEqualsChars(String string) {
-        int count = 0;
-        int sum = 0;
-        for (int i = 0; i < string.length() - 1; i++) {
-            if (string.charAt(i) == string.charAt(i + 1)) {
-                count++;
-            } else if (string.charAt(i) != string.charAt(i + 1) && count > 0) {
-                sum = sum + 1 + count;
-                count = 0;
+        try {
+            if (string.length() != 0) {
+                int count = 0;
+                int sum = 0;
+                for (int i = 0; i < string.length() - 1; i++) {
+                    if (string.charAt(i) == string.charAt(i + 1)) {
+                        count++;
+                    } else if (string.charAt(i) != string.charAt(i + 1) && count > 0) {
+                        sum = sum + 1 + count;
+                        count = 0;
+                    }
+                }
+                if (count != 0) {
+                    sum = sum + 1 + count;
+                }
+                return sum;
+            } else {
+                throw new IllegalArgumentException("Illegal argument");
             }
+        } catch (NullPointerException e) {
+            System.err.println("String is null");
         }
-        if (count != 0) {
-            sum = sum + 1 + count;
-        }
-        return sum;
+        throw new IllegalArgumentException("Illegal argument");
     }
 
     public static String swapSecondAndFifth(String string) {
-        char[] chars = string.toCharArray();
-        char temp = chars[1];
-        chars[1] = chars[4];
-        chars[4] = temp;
-        return String.copyValueOf(chars);
+        try {
+            if (string.length() < 5) {
+                char[] chars = string.toCharArray();
+                char temp = chars[1];
+                chars[1] = chars[4];
+                chars[4] = temp;
+                return String.copyValueOf(chars);
+            } else {
+                throw new IllegalArgumentException("Illegal argument");
+            }
+        } catch (NullPointerException e) {
+            System.err.println("String is null");
+        }
+        throw new IllegalArgumentException("Illegal argument");
     }
 
     public static String reverse(String string) {
-        char[] chars = string.toCharArray();
-        char[] result = new char[chars.length];
-        int resultIndex = 0;
-        for (int i = chars.length -1 ; i >= 0; i--) {
-            result[resultIndex] = chars[i];
-            resultIndex++;
+        try {
+            if (string.length() != 0) {
+                char[] chars = string.toCharArray();
+                char[] result = new char[chars.length];
+                int resultIndex = 0;
+                for (int i = chars.length - 1; i >= 0; i--) {
+                    result[resultIndex] = chars[i];
+                    resultIndex++;
+                }
+                return String.copyValueOf(result);
+            } else {
+                throw new IllegalArgumentException("Illegal argument");
+            }
+        } catch (NullPointerException e) {
+            System.err.println("String is null");
         }
-        return String.copyValueOf(result);
+        throw new IllegalArgumentException("Illegal argument");
     }
-
 
 }
